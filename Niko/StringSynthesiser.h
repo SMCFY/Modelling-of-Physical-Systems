ca3a46398e5bdb061b3d937ff46cafdad6f4a027
NikolajAndersson <nikolajandersson@gmail.com>
2017-03-06 11:45:12 +0100

    Added analog phasor effect to karplus

--- Niko/StringSynthesiser.h
+++ Niko/StringSynthesiser.h
@@ -19,6 +19,7 @@ public:
     */
     StringSynthesiser (double sampleRate, double frequencyInHz)
     {
+        fs = sampleRate;
         doPluckForNextBuffer.set (false);
         prepareSynthesiserState (sampleRate, frequencyInHz);
     }
@@ -61,7 +62,22 @@ public:
             exciteInternalBuffer();
         
         //std::cout << amplitude << "\n";
-
+        
+        float delta1 = freq1 * 2 * float_Pi / fs;
+        float delta2 = freq2 * 2 * float_Pi / fs;
+        float delta3 = freq3 * 2 * float_Pi / fs;
+        float delta4 = freq4 * 2 * float_Pi / fs;
+        
+        float a21 = pow(r1,2);
+        float a22 = pow(r2,2);
+        float a23 = pow(r3,2);
+        float a24 = pow(r4,2);
+        
+        float a11 = -2*r1*cos(phase1);
+        float a12 = -2*r2*cos(phase2);
+        float a13 = -2*r3*cos(phase3);
+        float a14 = -2*r4*cos(phase4);
+        
         // cycle through the delay line and apply a simple averaging filter + allpass
         for (int i = 0; i < numSamples; ++i)
         {
@@ -99,8 +115,57 @@ public:
             {
                 sample = -(2.0/3.0);
             }
+            // obj.AP1(1) = a21 * x(i,1) + a11 * obj.xn1 + obj.xn2 - a11 * obj.AP1(2) - a21 * obj.AP1(3);
+
+            // Add phasor
+            APphasor1[0] = a21 * sample + a11 * xn1 + xn2 - a11 * APphasor1[1] - a21 * APphasor1[2];
+            APphasor2[0] = a22 * APphasor1[0] + a12 * APphasor1[1] + APphasor1[2] - a12 * APphasor2[1] - a22 * APphasor2[2];
+            APphasor3[0] = a23 * APphasor2[0] + a13 * APphasor2[1] + APphasor2[2] - a13 * APphasor3[1] - a23 * APphasor3[2];
+            APphasor4[0] = a24 * APphasor3[0] + a14 * APphasor3[1] + APphasor3[2] - a14 * APphasor4[1] - a24 * APphasor4[2];
+
+            // Delay line stuff
+            xn2 = xn1;
+            xn1 = sample;
+            
+            APphasor1[2] = APphasor1[1];
+            APphasor1[1] = APphasor1[0];
+            
+            APphasor2[2] = APphasor2[1];
+            APphasor2[1] = APphasor2[0];
+            
+            APphasor3[2] = APphasor3[1];
+            APphasor3[1] = APphasor3[0];
+            
+            APphasor4[2] = APphasor4[1];
+            APphasor4[1] = APphasor4[0];
+            
+            // increment phase
+            phase1 += delta1;
+            if(phase1 > 2*float_Pi)
+                phase1 -= 2*float_Pi;
+            
+            phase2 += delta2;
+            if(phase2 > 2*float_Pi)
+                phase2 -= 2*float_Pi;
+
+            phase3 += delta3;
+            if(phase3 > 2*float_Pi)
+                phase3 -= 2*float_Pi;
+
+            phase4 += delta4;
+            if(phase4 > 2*float_Pi)
+                phase4 -= 2*float_Pi;
+
+            
+            // Calculate new coefficient
+            a11 = -2*r1*cos(phase1);
+            a12 = -2*r1*cos(phase2);
+            a13 = -2*r1*cos(phase3);
+            a14 = -2*r1*cos(phase4);
+            
             //outBuffer[i] += delayLine[pos]; // normal karplus
-            outBuffer[i] += sample; // distortion
+            //outBuffer[i] += sample; // distortion
+            outBuffer[i] += 0.5*sample+0.5*APphasor4[0]; // with phasor
             //outBuffer[i] += delayLine[tap1]+(-delayLine[tap2]) // pluck pos;
             pos = nextPos;
         }
@@ -150,7 +215,33 @@ private:
     float APdelay[2] = {0,0};
     
     float m = 0.5;
-    int d = 2;
+    int d = 1;
+    
+    // Analog phaser
+    float APphasor1[3] = {0,0,0};
+    float APphasor2[3] = {0,0,0};
+    float APphasor3[3] = {0,0,0};
+    float APphasor4[3] = {0,0,0};
+    
+    float xn1 = 0;
+    float xn2 = 0;
+    
+    float phase1 = 0;
+    float phase2 = 0;
+    float phase3 = 0;
+    float phase4 = 0;
+    
+    float r1 = 0.8;
+    float r2 = 0.8;
+    float r3 = 0.8;
+    float r4 = 0.8;
+    
+    float freq1 = 0.5;
+    float freq2 = 2;
+    float freq3 = 3;
+    float freq4 = 5;
+    
+    int fs = 44100;
     
     Atomic<int> doPluckForNextBuffer;
 
