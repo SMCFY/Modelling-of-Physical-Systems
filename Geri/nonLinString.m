9e224233443e2162b3cb569c7c5406ed038869da
gericsapo <gericsapo@gmail.com>
2017-06-13 00:31:42 +0200

    delay line redecleared, sinewave as modulatingsignal, nonlinear filter still does not work

--- Geri/nonLinString.m
+++ Geri/nonLinString.m
@@ -28,14 +28,13 @@ function y = nonLinString (f0, d, g, a1, a2)
 	fs = 44100;
 	%f0 = 440;
 	L = floor(fs/f0);
-	del = zeros(L,1); % z^-L
-    delPast = 0; % x[n-1]
+	del = zeros(L+1,1); % z^-L
     
 	% I/O
 	t = 1;
 	x = zeros(t*fs,1); x(1:500) = rand(500,1); x(1:500) = x(1:500)-mean(x(1:500)); %input
 	y = zeros(t*fs,1); %output
-    pmSin = sin(2*pi*6*[0:1/fs:1])';
+    pmSin = sin(2*pi*60*[0:1/fs:1])';
 	
 	%filters (buffers and coefficients)
 	nonL = [0 0]; %non-linearity filter(APF) 	y[n] = a0x[n]+x[n-1]-a0y[n-1]	
@@ -54,19 +53,18 @@ function y = nonLinString (f0, d, g, a1, a2)
 	for i=1:length(y)
         %a0 = pmSin(i);        
 
-		del(2:L) = del(1:L-1); %shifting values from left to right
+		del(2:L+1) = del(1:L); %shifting values from left to right
 		del(1) = x(i)+loss(1); %writing to the delay line
 	
-        delPast = del(L); %store past input sample x[n-1]
 		nonL(2) = nonL(1); % shifting the values in the buffer
-		nonL(1) = a0*del(L)+delPast-a0*nonL(2); %reading the delay lines last sample(x[n]) + its past sample(x[n-1])
+		nonL(1) = a0*del(L)+del(L+1)-a0*nonL(2); %reading the delay lines last sample(x[n]) + its past sample(x[n-1])
 
         %coefficient modulation
 		w(1) = del(L)-a0*nonL(1);
 		%f = (1/(1-a0))*(w(1)+w(2));
-	    if sign(w(1)) == 1 %change the filter coefficient on sign change
+	    if sign(pmSin(i)) == 1 %change the filter coefficient on sign change
 	        a0 = k(1);
-	    elseif sign(w(1)) == -1
+	    elseif sign(pmSin(i)) == -1
 	        a0 = k(2);
 	    end
 	   
@@ -82,5 +80,4 @@ function y = nonLinString (f0, d, g, a1, a2)
 	
 	y = y/max(y);
 	sound(y, fs);
-    %figure; plot(abs(fft(y)));
 end
